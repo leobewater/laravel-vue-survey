64bfc01200c9de313b2669d209467d8fed8d6b2c
Leo <lcheung@swellbottle.com>
2022-03-04 10:13:30 -0500

    set up user in vuex and populate in app

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,12 +1,15 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['user']),
+  },
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <pre>{{ user }}</pre>
 </template>
 
 <style>
