386473687a922d69da312bb340736102b1dfad56
Leo <lcheung@swellbottle.com>
2022-03-04 14:27:18 -0500

    remove token when logout

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -27,6 +27,7 @@ const store = createStore({
     logout: (state) => {
       state.user.data = {}
       state.user.token = null
+      sessionStorage.removeItem('TOKEN')
     },
     setUser: (state, userData) => {
       state.user.token = userData.token
