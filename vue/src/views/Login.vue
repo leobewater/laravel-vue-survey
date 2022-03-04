8eb347d61682192362af22bb29726d79a47f6543
Leo <lcheung@swellbottle.com>
2022-03-04 14:23:03 -0500

    add error handling for login form

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -19,6 +19,32 @@
     </p>
   </div>
   <form class="mt-8 space-y-6" @submit="login">
+    <div
+      v-if="errorMsg"
+      class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded"
+    >
+      {{ errorMsg }}
+      <span
+        @click="errorMsg = ''"
+        class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </span>
+    </div>
+
     <input type="hidden" name="remember" value="true" />
     <div class="rounded-md shadow-sm -space-y-px">
       <div>
@@ -94,21 +120,30 @@
 <script setup>
 import store from '../store'
 import { useRouter } from 'vue-router'
+import { ref } from 'vue'
 
 const router = useRouter()
 
 const user = {
   email: '',
   password: '',
+  remember: false,
 }
+
+let errorMsg = ref('')
 
 function login(ev) {
   ev.preventDefault()
 
-  store.dispatch('login', user).then(() => {
-    router.push({
-      name: 'Dashboard',
+  store
+    .dispatch('login', user)
+    .then(() => {
+      router.push({
+        name: 'Dashboard',
+      })
     })
-  })
+    .catch((err) => {
+      errorMsg.value = err.response.data.error
+    })
 }
 </script>
