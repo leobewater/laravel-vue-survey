f2a271cbe7256e6fcd85f2084f1601b4f87514b4
Leo <lcheung@swellbottle.com>
2022-03-04 11:14:17 -0500

    add auth layout and move login and register as children

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,6 +4,7 @@ import Surveys from '../views/Surveys.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
 import DefaultLayout from '../components/DefaultLayout.vue'
+import AuthLayout from '../components/AuthLayout.vue'
 import store from '../store'
 
 const routes = [
@@ -27,14 +28,22 @@ const routes = [
     ],
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login,
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register,
+    path: '/auth',
+    redirect: '/login',
+    name: 'Auth',
+    component: AuthLayout,
+    children: [
+      {
+        path: '/login',
+        name: 'Login',
+        component: Login,
+      },
+      {
+        path: '/register',
+        name: 'Register',
+        component: Register,
+      },
+    ],
   },
 ]
 
