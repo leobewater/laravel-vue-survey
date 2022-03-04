ae3c6e885a5183d80715b5476219f6a1d6b2c8ec
Leo <lcheung@swellbottle.com>
2022-03-04 15:05:34 -0500

    add surveys create and detail routes

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
 import Surveys from '../views/Surveys.vue'
+import SurveyView from '../views/SurveyView.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
 import DefaultLayout from '../components/DefaultLayout.vue'
@@ -24,6 +25,16 @@ const routes = [
         path: '/surveys',
         name: 'Surveys',
         component: Surveys,
+      },
+      {
+        path: '/surveys/create',
+        name: 'SurveyCreate',
+        component: SurveyView,
+      },
+      {
+        path: '/surveys/:id',
+        name: 'SurveyView',
+        component: SurveyView,
       },
     ],
   },
