0384e9950aa5a8e7b8ce90d570fdefdd1a4a4bc2
Leo <lcheung@swellbottle.com>
2022-03-04 15:08:18 -0500

    add title for survey create page

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    
-  </div>
+  <PageComponent title="View Or Create A Survey">
+    View or create a survey
+  </PageComponent>
 </template>
+
 <script setup>
+import PageComponent from '../components/PageComponent.vue'
 </script>
