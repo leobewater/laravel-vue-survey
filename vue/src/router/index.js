33b8093338272fdd289a726dc43f234713931494
Leo <lcheung@swellbottle.com>
2022-03-04 11:00:14 -0500

    set up default layout component and routes

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,12 +2,21 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
+import DefaultLayout from '../components/DefaultLayout.vue'
 
 const routes = [
   {
     path: '/',
+    redirect: '/dashboard',
     name: 'Dashboard',
-    component: Dashboard,
+    component: DefaultLayout,
+    children: [
+     {
+       path: '/dashboard',
+       name: 'Dashboard',
+       component: Dashboard
+     } 
+    ]
   },
   {
     path: '/login',
