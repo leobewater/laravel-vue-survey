57d080a0e5f110a935c8eadce805bd2e1ed1cffd
Leo <lcheung@swellbottle.com>
2022-03-04 13:48:18 -0500

    register user and set store user

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -18,7 +18,7 @@
       </router-link>
     </p>
   </div>
-  <form class="mt-8 space-y-6" action="#" method="POST">
+  <form class="mt-8 space-y-6" @submit="register">
     <input type="hidden" name="remember" value="true" />
     <div class="rounded-md shadow-sm -space-y-px">
       <div>
@@ -29,6 +29,7 @@
           type="name"
           autocomplete="name"
           required
+          v-model="user.name"
           class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
           placeholder="Full name"
         />
@@ -41,6 +42,7 @@
           type="email"
           autocomplete="email"
           required
+          v-model="user.email"
           class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
           placeholder="Email address"
         />
@@ -53,8 +55,24 @@
           type="password"
           autocomplete="current-password"
           required
-          class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+          v-model="user.password"
+          class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
           placeholder="Password"
+        />
+      </div>
+      <div>
+        <label for="password_confirmation" class="sr-only"
+          >Password Confirmation</label
+        >
+        <input
+          id="password_confirmation"
+          name="password_confirmation"
+          type="password_confirmation"
+          autocomplete="current-password_confirmation"
+          required
+          v-model="user.password_confirmation"
+          class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+          placeholder="Password Confirmation"
         />
       </div>
     </div>
@@ -85,6 +103,25 @@
     </div>
   </form>
 </template>
-<script>
-export default {}
+
+<script setup>
+import store from '../store'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const user = {
+  name: '',
+  email: '',
+  password: '',
+  password_confirmation: '',
+}
+
+function register(ev) {
+  ev.preventDefault()
+
+  // dispatch action
+  store.dispatch('register', user).then((res) => {
+    router.push({ name: 'Dashboard' })
+  })
+}
 </script>
