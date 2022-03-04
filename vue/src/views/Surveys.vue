8d1c2a15a6667fa3dbb06d25a94d8d922684ac94
Leo <lcheung@swellbottle.com>
2022-03-04 15:17:10 -0500

    populate survey on surveys view

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -23,7 +23,38 @@
         >
       </div>
     </template>
-    <div>
+
+    <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols3">
+      <div
+        v-for="survey in surveys"
+        :key="survey.id"
+        class="flex flex-col py-4 px-6 shadow-md bg-white hover:bg-gray-50 h-[470px]"
+      >
+        <img :src="survey.image" alt="" class="w-full h-48 object-cover" />
+        <h4 class="mt-4 text-lg font-bold">{{ survey.title }}</h4>
+        <div v-html="survey.description" class="overflow-hidden flex-1"></div>
+
+        <div class="flex justify-between items-center mt-3">
+          <router-link
+            :to="{ name: 'SurveyView', params: { id: survey.id } }"
+            class="flex py-2 px-4 border border-transparent text-sm rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5 mr-2"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+              /></svg
+            >Edit</router-link
+          >
+        </div>
+      </div>
     </div>
   </PageComponent>
 </template>
