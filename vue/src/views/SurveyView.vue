7a66c06230afbdf24616ec6624bb53b97d312a48
Leo <lcheung@swellbottle.com>
2022-03-05 08:06:10 -0500

    add save button

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -7,6 +7,9 @@
         </h1>
       </div>
 
+      <pre>
+        {{ model }}
+      </pre>
       <form @submit.prevent="saveSurvey">
         <div class="shadow sm:rounded-md sm:overflow-hidden">
           <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
@@ -117,8 +120,16 @@
                 >
               </div>
             </div>
+            <!-- / survey fields -->
 
-            <!-- end survey fields -->
+            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+              <button
+                type="submit"
+                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Save
+              </button>
+            </div>
           </div>
         </div>
       </form>
