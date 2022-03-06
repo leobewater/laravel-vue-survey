4a3a6cf1d8b26a410fbdba0fc653da6c974e134e
Leo <lcheung@swellbottle.com>
2022-03-06 18:03:29 -0500

    add save survey answer method and route

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Survey;
+use App\Models\SurveyAnswer;
+use App\Models\SurveyQuestion;
+use App\Models\SurveyQuestionAnswer;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use App\Models\SurveyQuestion;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Validator;
 use App\Http\Resources\SurveyResource;
 use App\Http\Requests\StoreSurveyRequest;
-use Illuminate\Support\Facades\Validator;
 use App\Http\Requests\UpdateSurveyRequest;
+use App\Http\Requests\StoreSurveyAnswerRequest;
 
 class SurveyController extends Controller
 {
@@ -79,6 +82,36 @@ class SurveyController extends Controller
     public function showForGuest(Survey $survey)
     {
         return new SurveyResource($survey);
+    }
+
+    // save survey answers
+    public function storeAnswer(StoreSurveyAnswerRequest $request, Survey $survey)
+    {
+        $validated = $request->validated();
+
+        $surveyAnswer = SurveyAnswer::create([
+            'survey_id' => $survey->id,
+            'start_date' => date('Y-m-d H:i:s'),
+            'end_date' => date('Y-m-d H:i:s'),
+        ]);
+
+        foreach ($validated['answers'] as $questionId => $answer) {
+            // validate survey question id
+            $question = SurveyQuestion::where(['id' => $questionId, 'survey_id' => $survey->id])->get();
+            if (!$question) {
+                return response(`Invalid Question ID: "{$questionId}"`, 400);
+            }
+
+            $data = [
+                'survey_question_id' => $questionId,
+                'survey_answer_id' => $surveyAnswer->id,
+                'answer' => is_array($answer) ? json_encode($answer) : $answer
+            ];
+
+            SurveyQuestionAnswer::create($data);
+        }
+
+        return response('', 201);
     }
 
     /**
