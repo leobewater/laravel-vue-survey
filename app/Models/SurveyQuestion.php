421c93add2df3ef6dee842ce4b001ab89ee9f54d
Leo <lcheung@swellbottle.com>
2022-03-06 14:45:27 -0500

    add survey question fillable

--- app/Models/SurveyQuestion.php
+++ app/Models/SurveyQuestion.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class SurveyQuestion extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'question', 'data', 'type', 'survey_id', 'description'
+    ];
 }
