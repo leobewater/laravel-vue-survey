772c77938488460f5eedbe8e6d8d1a78ef493096
Leo <lcheung@swellbottle.com>
2022-03-06 07:36:09 -0500

    add sluggable to survey model

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Survey extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'status',
+        'description',
+        'expire_date',
+    ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
