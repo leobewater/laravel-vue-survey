eee9935effbe5e2255400dd3ddc6eb5801575ded
Leo <lcheung@swellbottle.com>
2022-03-06 17:11:08 -0500

    add public link to survey listing item

--- vue/src/components/SurveyListItem.vue
+++ vue/src/components/SurveyListItem.vue
@@ -2,7 +2,11 @@
   <div
     class="flex flex-col py-4 px-6 shadow-md bg-white hover:bg-gray-50 h-[470px]"
   >
-    <img :src="survey.image_url" alt="" class="w-full h-48 object-cover" />
+    <img
+      :src="survey.image_url"
+      :alt="survey.title"
+      class="w-full h-48 object-cover"
+    />
     <h4 class="mt-4 text-lg font-bold">{{ survey.title }}</h4>
     <div v-html="survey.description" class="overflow-hidden flex-1"></div>
 
@@ -25,27 +29,49 @@
           /></svg
         >Edit</router-link
       >
-      <button
-        v-if="survey.id"
-        type="button"
-        @click="emit('delete', survey)"
-        class="h-8 w-8 flex items-center justify-center rounded-full border border-transparent text-sm text-red-500 focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5 -mt-1 inline-block"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
+      <div class="flex items-center">
+        <a
+          :href="`/view/survey/${survey.slug}`"
+          target="_blank"
+          class="h-8 w-8 flex items-center justify-center rounded-full border border-transparent text-sm text-indigo-500 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-          />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+            />
+          </svg>
+        </a>
+        <button
+          v-if="survey.id"
+          type="button"
+          @click="emit('delete', survey)"
+          class="h-8 w-8 flex items-center justify-center rounded-full border border-transparent text-sm text-red-500 focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5 -mt-1 inline-block"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   </div>
 </template>
