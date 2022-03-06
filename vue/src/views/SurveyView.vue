c594eb4b78ec101e56011ec679dfc74e4cdf04c2
Leo <lcheung@swellbottle.com>
2022-03-06 16:25:11 -0500

    add public link to survey single

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -6,33 +6,57 @@
           {{ route.params.id ? model.title : 'Create a Survey' }}
         </h1>
 
-        <button
-          v-if="route.params.id"
-          type="button"
-          @click="deleteSurvey"
-          class="py-2 px-3 text-white bg-red-500 rounded-md hover:bg-red-600"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5 -mt-1 inline-block"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
+        <div class="flex">
+          <a
+            :href="`/view/survey/${model.slug}`"
+            target="_blank"
+            v-if="model.slug"
+            class="flex py-2 px-4 border border-transparent text-sm rounded-md text-indigo-500 hover:bg-indigo-700 hover:text-white transition-colors focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mr-2"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-            />
-          </svg>
-          Delete Survey
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+              />
+            </svg>
+            View Public link
+          </a>
+          <button
+            v-if="route.params.id"
+            type="button"
+            @click="deleteSurvey"
+            class="py-2 px-3 text-white bg-red-500 rounded-md hover:bg-red-600"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5 -mt-1 inline-block"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              />
+            </svg>
+            Delete Survey
+          </button>
+        </div>
       </div>
     </template>
 
     <div v-if="surveyLoading" class="flex justify-center">Loading...</div>
-    <form v-else @submit.prevent="saveSurvey">
+    <form v-else @submit.prevent="saveSurvey" class="animate-fade-in-down">
       <div class="shadow sm:rounded-md sm:overflow-hidden">
         <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
           <!-- Image -->
@@ -225,6 +249,7 @@ const surveyLoading = computed(() => store.state.currentSurvey.loading)
 // create empty survey
 let model = ref({
   title: '',
+  slug: '',
   status: false,
   description: null,
   // image: null,
@@ -239,7 +264,7 @@ watch(
   (newVal, oldVal) => {
     model.value = {
       ...JSON.parse(JSON.stringify(newVal)),
-      status: newVal.status !== 'draft',
+      status: !!newVal.status,
     }
   }
 )
@@ -263,6 +288,7 @@ function onImageChoose(ev) {
 
     // for display only
     model.value.image_url = reader.result
+    ev.target.value = ''
   }
   reader.readAsDataURL(file)
 }
@@ -288,6 +314,12 @@ function deleteQuestion(question) {
 }
 
 function questionChange(question) {
+  // Important to explicitelly assign question.data.options, because otherwise it is a Proxy object
+  // and it is lost in JSON.stringify()
+  if (question.data.options) {
+    question.data.options = [...question.data.options]
+  }
+
   model.value.questions = model.value.questions.map((q) => {
     if (q.id === question.id) {
       return JSON.parse(JSON.stringify(question))
@@ -297,7 +329,16 @@ function questionChange(question) {
 }
 
 function saveSurvey() {
+  let action = 'created'
+  if (model.value.id) {
+    action = 'updated'
+  }
   store.dispatch('saveSurvey', model.value).then(({ data }) => {
+    store.commit('notify', {
+      type: 'success',
+      message: 'The survey was successfully ' + action,
+    })
+
     router.push({
       name: 'SurveyView',
       params: { id: data.data.id },
