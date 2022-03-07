a8ba0dcb13d8044ea3500a145363f0043cbe402f
Leo <lcheung@swellbottle.com>
2022-03-07 11:08:03 -0500

    add alert component and spinning icon to login

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -10,6 +10,7 @@
     </h2>
     <p class="mt-2 text-center text-sm text-gray-600">
       Or
+      {{ " " }}
       <router-link
         :to="{ name: 'Register' }"
         class="font-medium text-indigo-600 hover:text-indigo-500"
@@ -19,10 +20,7 @@
     </p>
   </div>
   <form class="mt-8 space-y-6" @submit="login">
-    <div
-      v-if="errorMsg"
-      class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded"
-    >
+    <Alert v-if="errorMsg">
       {{ errorMsg }}
       <span
         @click="errorMsg = ''"
@@ -43,8 +41,7 @@
           />
         </svg>
       </span>
-    </div>
-
+    </Alert>
     <input type="hidden" name="remember" value="true" />
     <div class="rounded-md shadow-sm -space-y-px">
       <div>
@@ -93,24 +90,40 @@
     <div>
       <button
         type="submit"
+        :disabled="loading"
         class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        :class="{
+          'cursor-not-allowed': loading,
+          'hover:bg-indigo-500': loading,
+        }"
       >
         <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-          <!-- Heroicon name: solid/lock-closed -->
-          <svg
+          <LockClosedIcon
             class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
             aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          />
         </span>
+        <svg
+          v-if="loading"
+          class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
         Sign in
       </button>
     </div>
@@ -118,9 +131,11 @@
 </template>
 
 <script setup>
-import store from '../store'
-import { useRouter } from 'vue-router'
-import { ref } from 'vue'
+import { LockClosedIcon } from "@heroicons/vue/solid";
+import store from "../store";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+import Alert from "../components/Alert.vue";
 
 const router = useRouter()
 
@@ -129,21 +144,24 @@ const user = {
   password: '',
   remember: false,
 }
-
+let loading = ref(false);
 let errorMsg = ref('')
 
 function login(ev) {
   ev.preventDefault()
 
+  loading.value = true;
   store
     .dispatch('login', user)
     .then(() => {
+      loading.value = false;
       router.push({
         name: 'Dashboard',
       })
     })
     .catch((err) => {
-      errorMsg.value = err.response.data.error
-    })
+      loading.value = false;
+      errorMsg.value = err.response.data.error;
+    });
 }
 </script>
