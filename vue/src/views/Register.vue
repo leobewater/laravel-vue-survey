a4fed0958339d0413ddac3b5616bd7bd00e8dbee
Leo <lcheung@swellbottle.com>
2022-03-07 11:15:23 -0500

    add spinning icon and show error messages

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -10,6 +10,7 @@
     </h2>
     <p class="mt-2 text-center text-sm text-gray-600">
       Or
+      {{ " " }}
       <router-link
         :to="{ name: 'Login' }"
         class="font-medium text-indigo-600 hover:text-indigo-500"
@@ -19,6 +20,14 @@
     </p>
   </div>
   <form class="mt-8 space-y-6" @submit="register">
+    <Alert v-if="Object.keys(errors).length" class="flex-col items-stretch text-sm">
+      <div v-for="(field, i) of Object.keys(errors)" :key="i">
+        <div v-for="(error, ind) of errors[field] || []" :key="ind">
+          * {{ error }}
+        </div>
+      </div>
+    </Alert>
+
     <input type="hidden" name="remember" value="true" />
     <div class="rounded-md shadow-sm -space-y-px">
       <div>
@@ -26,7 +35,7 @@
         <input
           id="fullname"
           name="name"
-          type="name"
+          type="text"
           autocomplete="name"
           required
           v-model="user.name"
@@ -44,6 +53,7 @@
           required
           v-model="user.email"
           class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+          :class="{ 'border-red-500': errors.email, 'z-10': errors.email }"
           placeholder="Email address"
         />
       </div>
@@ -58,6 +68,7 @@
           v-model="user.password"
           class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
           placeholder="Password"
+          :class="{ 'border-red-500': errors.password, 'z-10': errors.password }"
         />
       </div>
       <div>
@@ -72,7 +83,7 @@
           required
           v-model="user.password_confirmation"
           class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-          placeholder="Password Confirmation"
+          placeholder="Confirm Password"
         />
       </div>
     </div>
@@ -80,24 +91,40 @@
     <div>
       <button
         type="submit"
+        :disabled="loading"
         class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        :class="{
+          'cursor-not-allowed': loading,
+          'hover:bg-indigo-500': loading,
+        }"
       >
         <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-          <!-- Heroicon name: solid/lock-closed -->
-          <svg
+          <LockClosedIcon
             class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
             aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          />
         </span>
+        <svg
+          v-if="loading"
+          class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
         Sign up
       </button>
     </div>
@@ -105,8 +132,11 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
+import { LockClosedIcon } from "@heroicons/vue/solid";
 import store from '../store'
 import { useRouter } from 'vue-router'
+import Alert from "../components/Alert.vue";
 
 const router = useRouter()
 const user = {
@@ -115,13 +145,25 @@ const user = {
   password: '',
   password_confirmation: '',
 }
+const loading = ref(false);
+const errors = ref({});
 
 function register(ev) {
-  ev.preventDefault()
-
-  // dispatch action
-  store.dispatch('register', user).then((res) => {
-    router.push({ name: 'Dashboard' })
-  })
+  ev.preventDefault();
+  loading.value = true;
+  store
+    .dispatch("register", user)
+    .then(() => {
+      loading.value = false;
+      router.push({
+        name: "Dashboard",
+      });
+    })
+    .catch((error) => {
+      loading.value = false;
+      if (error.response.status === 422) {
+        errors.value = error.response.data.errors;
+      }
+    });
 }
 </script>
