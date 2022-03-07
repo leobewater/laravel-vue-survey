7e1c067baaefe14a6aaffcffc0a6e9c2c2aca29b
Leo <lcheung@swellbottle.com>
2022-03-07 10:38:11 -0500

    add dashboard route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SurveyController;
 
 /*
@@ -27,6 +28,8 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::resource('/survey', SurveyController::class);
+
+    Route::get('/dashboard', [DashboardController::class, 'index']);
 });
 
 Route::get('/survey-by-slug/{survey:slug}', [SurveyController::class, 'showForGuest']);
