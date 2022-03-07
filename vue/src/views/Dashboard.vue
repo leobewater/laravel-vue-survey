0d6e84e41b9808b3ed941133c58b3024cccf3f6c
Leo <lcheung@swellbottle.com>
2022-03-07 10:41:42 -0500

    load dashboard data from api

--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -1,7 +1,18 @@
 <template>
   <PageComponent title="Dashboard">Content goes here</PageComponent>
+  {{ loading }}
+  {{ data }}
 </template>
 
 <script setup>
 import PageComponent from '../components/PageComponent.vue'
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+const loading = computed(() => store.state.dashboard.loading)
+const data = computed(() => store.state.dashboard.data)
+
+store.dispatch('getDashboardData')
 </script>
